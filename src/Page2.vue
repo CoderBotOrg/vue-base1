cbe7911dccbf9ae91bc130f7321948d656e9d73d
Antonio Vivace <puma95.95@gmail.com>
2018-04-04 11:29:37 +0200

    Vue-router configuration, dynamic routing example

---
+++ src/Page2.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1> Page 2 </h1>
+    {{ $route.params.id }}
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import Page1 from './Page1.vue'
+import Page2 from './Page2.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/foo', component: Page1 },
+  { path: '/bar', component: Page2 },
+  { path: '/user/:id', component: Page2}
+]
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
 
 new Vue({
+  router,
   el: '#app',
   render: h => h(App)
 })
